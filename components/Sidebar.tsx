29a1a99f1965f67073c54d1e6c36d2c956549baa
t_miyazaki <t_miyazaki@sunbit.co.jp>
2025-12-04 16:18:00 +0900

    feat: Add sidebar controls for ticket and project management in timeline view

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,14 +20,15 @@ const menuItems: MenuItem[] = [
 interface SidebarProps {
   activeItem: MenuItemId;
   onItemClick: (id: MenuItemId) => void;
+  extraContent?: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ activeItem, onItemClick }) => {
+const Sidebar: React.FC<SidebarProps> = ({ activeItem, onItemClick, extraContent }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <aside className="w-56 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 flex flex-col flex-shrink-0 transition-colors duration-200">
-      <nav className="flex-1 py-4">
+      <nav className="flex-1 py-4 overflow-y-auto">
         <ul className="space-y-1 px-3">
           {menuItems.map((item) => {
             const Icon = item.icon;
@@ -50,6 +51,13 @@ const Sidebar: React.FC<SidebarProps> = ({ activeItem, onItemClick }) => {
           })}
         </ul>
       </nav>
+
+      {extraContent && (
+        <div className="p-3 border-t border-gray-200 dark:border-gray-700">
+          {extraContent}
+        </div>
+      )}
+
       <div className="p-3 border-t border-gray-200 dark:border-gray-700">
         <button
           onClick={toggleTheme}
